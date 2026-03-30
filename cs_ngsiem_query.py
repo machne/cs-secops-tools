dc59d301cd6880c834d5d14238c2154bae7bd110
john fort <jpfort@gmail.com>
2026-03-30 08:57:07 -0400

    rever url

--- cs_ngsiem_query.py
+++ cs_ngsiem_query.py
@@ -42,7 +42,7 @@ POLL_INTERVAL = 5
 MAX_WAIT      = 120
 
 # SecOps
-SECOPS_INGEST_URL = "https://us-malachiteingestion-pa.googleapis.com/v2/unstructuredlogentries:batchCreate"
+SECOPS_INGEST_URL = "https://malachiteingestion-pa.googleapis.com/v2/unstructuredlogentries:batchCreate"
 SECOPS_SCOPE      = "https://www.googleapis.com/auth/cloud-platform"
 
 
